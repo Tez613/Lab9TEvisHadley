e4f4ebca0a88c361abcfeb0f246a61e56f76922d
Tez613 <tevishadley@gmail.com>
2022-11-04 19:57:35 -0600

    almost done just one more exception to handle

--- Program.cs
+++ Program.cs
@@ -7,11 +7,11 @@ while (Exit < 1)
 {
     Console.Clear();
     Console.WriteLine("This is a to do list. \nPlease type in a number to select an option.");
-    Console.WriteLine("What would you like to do? \nYour options are: \n 1: View the entire list \n 2: View all of the to do items  \n 3: View all of the completed items \n 4: Add an item to the to do list \n 5: Mark a item as completed \n 6: Stats \n 7: Exit");
-    int choice = int.Parse(Console.ReadLine());
+    Console.WriteLine("What would you like to do? \nYour options are: \n 1: View the entire list \n 2: View all of the to do items  \n 3: View all of the completed items \n 4: Add an item to the to do list \n 5: Mark a item as completed \n 6: View the stats \n 7: Exit");
+    string choice = Console.ReadLine();
     switch (choice)
     {
-        case 1:
+        case "1":
             index = 1;
             Console.Clear();
             foreach (String a in ToDo)
@@ -24,7 +24,7 @@ while (Exit < 1)
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
             break;
-        case 2:
+        case "2":
             index = 0;
             Console.Clear();
             foreach (String a in ToDo)
@@ -32,7 +32,7 @@ while (Exit < 1)
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
             break;
-        case 3:
+        case "3":
             index = 0;
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Green;
@@ -42,7 +42,7 @@ while (Exit < 1)
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
             break;
-        case 4:
+        case "4":
             index = 0;
             DateTime StartTime = DateTime.Now;
             Console.Clear();
@@ -50,30 +50,43 @@ while (Exit < 1)
             ToDo.Add(Console.ReadLine() + " :Started on " + StartTime);
             index++;
             break;
-        case 5:
+        case "5":
             index = 0;
             Console.Clear();
-            Console.WriteLine("please choose an item to mark as completed by typing in its corresponding number.");
+            Console.WriteLine("please choose an item to mark as completed by typing in its corresponding number.\nTo Go back Type Back.");
             DateTime FinishedTime = DateTime.Now;
             foreach (String a in ToDo)
                 Console.WriteLine("To do item " + index++ + ": " + a);
-            int ListChoice = int.Parse(Console.ReadLine());
-            ToDoDone.Add(ToDo[ListChoice] + (": Finished on: ") + FinishedTime);
-            ToDo.RemoveAt(ListChoice);
+            string ListChoice = Console.ReadLine();
+            if (ListChoice == "Back")
+            {
+                break;
+            }
+            else
+            {
+                int ListChoice2 = int.Parse(ListChoice);
+                ToDoDone.Add(ToDo[ListChoice2] + (": Finished on: ") + FinishedTime);
+                ToDo.RemoveAt(ListChoice2);
+            }
             break;
-        case 6:
+        case "6":
             Console.Clear();
             int ToDoSize = ToDo.Count;
             int ToDoDoneSize = ToDoDone.Count;
             int TotalSize = ToDoSize + ToDoDoneSize;
             Console.WriteLine("Stats");
             Console.WriteLine(" There are " + TotalSize + " total items. \n There are " + ToDoSize + " items to do. \n there are " + ToDoDoneSize + " items that are done.");
-            Console.WriteLine("Press anykey to continue");
+            Console.WriteLine("Press any key to continue");
             Console.ReadKey();
             break;
-        case 7:
+        case "7":
             Exit++;
             Console.Clear();
+            break;
+        default:
+            Console.Clear();
+            Console.WriteLine("Incorrect option. Press any key to try again");
+            Console.ReadKey();
             break;
     }
 }
